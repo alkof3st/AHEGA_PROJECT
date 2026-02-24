3c5d56e7011e5b54b1f05c471492d059ace671e7
Разработчик Тамагочи <tamagotchi@dev.local>
2026-02-24 15:00:00 +0300

    Добавлен класс питомца с базовыми атрибутами

---
+++ src/pet.py
@@ -0,0 +1,12 @@
+class Pet:
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 50
+        self.cleanliness = 50
+        self.energy = 50
+        self.happiness = 50
+        self.coins = 50
+        self.inventory = []
+        self.outfit = None
+        self.position = (3, 3)
+        self.alive = True
